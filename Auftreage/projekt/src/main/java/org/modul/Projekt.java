fc7bd34035417e898a54a9eabccc19610f28d2e8
batu <batu.s@live.com>
2024-08-30 10:05:38 +0200

    Repetitions Auftrag: Guessing Game

--- Auftreage/projekt/src/main/java/org/modul/Projekt.java
+++ Auftreage/projekt/src/main/java/org/modul/Projekt.java
@@ -3,9 +3,7 @@
  */
 package org.modul;
 
-import java.util.ArrayList;
-
-import org.modul.Mediensammlung.Filme;
+import java.util.Scanner;
 
 /**
  *
@@ -25,23 +23,43 @@ public class Projekt {
 
         heizung.set_temp(30); */
 
+ /*   
         Filme godfather = new Filme("Godfather", "Action", 1970, 20, "German", "Paramount", 5, "DVD", "A movie about a mafia family");
         Filme godfather2 = new Filme("Godfather 2", "Action", 1972, 20, "German", "Paramount", 5, "DVD", "A movie about a mafia family");
         Filme godfather3 = new Filme("Godfather 3", "Action", 1974, 20, "German", "Paramount", 5, "DVD", "A movie about a mafia family");
 
-        ArrayList<Filme> col = new ArrayList<>();
+        ArrayList<Medien> col = new ArrayList<>();
 
         col.add(godfather);
         col.add(godfather2);
         col.add(godfather3);
 
         col.remove(godfather3);
-
+        
         // print all elements in the collection
-        for (Filme f : col) {
+        for (Medien f : col) {
             f.print();
             System.out.println("--------------------");
         }
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input your range (from 0 to ?): ");
+        String range = scanner.nextLine();
+        int int_range = 0;
 
+        try {
+            int_range = Integer.parseInt(range);
+
+        } catch (Exception e) {
+            System.out.println("Please input a number");
+            return;
+        }
+
+        RateSpiel rateSpiel = new RateSpiel(int_range);
+        while (rateSpiel.gamestatus) {
+            System.out.println("Your guess: ");
+            String guess = scanner.nextLine();
+            rateSpiel.check(guess);
+        }
     }
 }
---
+++ Auftreage/projekt/src/main/java/org/modul/RateSpiel.java
@@ -0,0 +1,52 @@
+package org.modul;
+
+import java.util.Random;
+
+public class RateSpiel {
+
+    public boolean gamestatus;
+    private int number;
+    private int range;
+
+    public RateSpiel(int range) {
+        if (range < 5) {
+            System.out.println("Number for range must be higher than 5.");
+            return;
+        }
+        this.range = range;
+        Random rand = new Random();
+        this.number = rand.nextInt(range + 1);
+        this.gamestatus = true;
+    }
+
+    void check(String n) {
+        switch (n.toLowerCase()) {
+            case "q" -> {
+                System.out.println("Quiting the game");
+                this.gamestatus = false;
+            }
+            default -> {
+                try {
+                    int number = Integer.parseInt(n);
+                    if (number == this.number) {
+                        System.out.println("You win!");
+                        this.gamestatus = false;
+                    }
+                    if (number < this.number) {
+                        System.out.println("Too small!");
+                    }
+                    if (number > this.number) {
+                        System.out.println("Too big!");
+                    }
+                    if (number > this.range + 1) {
+                        System.out.println("Please input a number between 0 and " + this.range);
+                    }
+                } catch (Exception e) {
+
+                    System.out.println("Please input a number between 0 and " + this.range);
+                }
+            }
+        }
+    }
+
+}
